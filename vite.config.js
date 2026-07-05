da1c11a2a22eebf3666ce09a988c0009c30e0f80
Rogue <blackleaner@gmail.com>
2026-07-06 01:58:54 +0700

    ada item rahasia pada vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+            ],
+            refresh: true, 
         }),
     ],
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: "10.228.193.14",
+        },
+    },
 });
